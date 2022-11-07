fdfae5399774e3deb3b1e16708d0c1fc6138f4e4
Danilo Costa <dancostasf@gmail.com>
2022-11-07 17:23:33 -0300

    commit

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,17 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val DATABASE_NAME = "marvel.db"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+
+    const val PUBLIC_KEY = "826a0df01e18d1190aa0f403598400af"
+    const val PRIVATE_KEY = "d0feaa9bf564fd09a7f689b143e0eb90b2e92eb9"
+
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+}
